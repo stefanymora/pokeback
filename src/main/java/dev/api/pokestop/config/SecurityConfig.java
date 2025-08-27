4a4bd5dc0416e0423df27b318361e771d0d797bb
stefanymora <estefani.morag@gmail.com>
2025-08-27 14:27:27 -0600

    cambios con security

---
+++ src/main/java/dev/api/pokestop/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package dev.api.pokestop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Desactivamos CSRF para que no bloquee peticiones
+                .cors(cors -> {
+                }) // Permitimos CORS
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Todas las rutas accesibles sin autenticación
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/")
+                        .allowedOrigins("http://localhost:3000", "https://pokestop-app.vercel.app") // Tu app en React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

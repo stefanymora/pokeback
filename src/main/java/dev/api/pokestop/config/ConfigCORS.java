5d7568fc0980523bde7288f8791ab28fdda4bf37
stefanymora <estefani.morag@gmail.com>
2025-09-05 13:52:36 -0600

    cambios CORSEABLES (local)

---
+++ src/main/java/dev/api/pokestop/config/ConfigCORS.java
@@ -0,0 +1,25 @@
+package dev.api.pokestop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigCORS {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://pokestop-app.vercel.app/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}

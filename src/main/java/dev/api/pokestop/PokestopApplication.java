43a972577536a40d37a70aabe62289e0d10a2226
stefanymora <estefani.morag@gmail.com>
2025-10-03 12:25:40 -0600

    ultimo cambio desde fani

--- src/main/java/dev/api/pokestop/PokestopApplication.java
+++ src/main/java/dev/api/pokestop/PokestopApplication.java
@@ -1,10 +1,11 @@
 package dev.api.pokestop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokestopApplication {
+public class   PokestopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokestopApplication.class, args);
